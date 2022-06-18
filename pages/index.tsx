8c5e45076b280d4ffcfbcc6e6266cd59ab9300f2
AL-AMIN-HUB <alaminsbl502@gmail.com>
2022-06-18 22:53:37 +0600

    added custom background

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ interface Props {
 }
 const Home = ({ netflixOriginals, actionMovies, comedyMovies, documentaries, horrorMovies, romanceMovies, topRated, trendingNow }: Props) => {
   return (
-    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
+    <div className="relative h-screen lg:h-[140vh]">
       <Head>
         <title>Home --Netflix</title>
         <link rel="icon" href="/favicon.ico" />
