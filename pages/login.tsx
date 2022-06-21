ac9088181cdef68cc1f1e26e30d42e67323e3fb5
alamincoders <alaminsbl502@gmail.com>
2022-06-21 13:42:19 +0600

    added react hook form

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,9 +1,30 @@
+import React from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
 import Head from "next/head";
 import Image from "next/image";
 import { useState } from "react";
 
+interface Inputs {
+  email: string;
+  password: string;
+}
+
 function login() {
   const [login, setLogin] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = async ({ email, password }) => {
+    if (login) {
+      // await signIn(email, password)
+    } else {
+      // await signUp(email, password);
+    }
+  };
 
   return (
     <section className="relative flex h-screen w-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent">
@@ -15,17 +36,21 @@ function login() {
 
       <img src="https://rb.gy/ulxxee" className="absolute left-4 top-4 cursor-pointer object-contain md:left-10 md:top-6" width={150} height={150} />
 
-      <form action="#" className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14">
+      <form onSubmit={handleSubmit(onSubmit)} className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14">
         <h1 className="text-4xl font-semibold">Sign In</h1>
         <div className="space-y-4">
           <label className="inline-block w-full">
-            <input type="email" placeholder="Email" className="input" />
+            <input {...register("email", { required: true })} type="email" placeholder="Email" className="input" />
+            {errors.email && <p className="p-1 text-[13px] font-light  text-orange-500">Please enter a valid email.</p>}
           </label>
           <label className="inline-block w-full">
-            <input type="password" placeholder="Password" className="input" />
+            <input {...register("password", { required: true })} type="password" placeholder="Password" className="input" />
+            {errors.password && (
+              <p className="p-1 text-[13px] font-light  text-orange-500">Your password must contain between 4 and 60 characters.</p>
+            )}
           </label>
         </div>
-        <button type="submit" className="w-full rounded bg-[#e50914] py-3 font-semibold">
+        <button onClick={() => setLogin(true)} type="submit" className="w-full rounded bg-[#e50914] py-3 font-semibold">
           Sign In
         </button>
         <div className="text-[grey]">
